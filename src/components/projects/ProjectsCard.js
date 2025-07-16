0eb523d50289d6d3ce6c05004f1cd2293136f1d2
Mikhael Fourie <mikhael.fourie@gmail.com>
2025-07-16 09:22:43 +0200

    Updated links to projects

--- src/components/projects/ProjectsCard.js
+++ src/components/projects/ProjectsCard.js
@@ -1,20 +1,25 @@
 import React from "react";
 
-const ProjectsCard = ({ image, title, category }) => {
+const ProjectsCard = ({ image, title, category, link }) => {
   return (
-    <div className="w-full py-8 flex flex-col justify-center items-center border-b-[1px] border-b-zinc-800">
-      <div className="w-full h-full mb-3 overflow-hidden relative cursor-pointer group">
+    <div className='w-full py-8 flex flex-col justify-center items-center border-b-[1px] border-b-zinc-800'>
+      <a
+        href={link}
+        target='_blank'
+        rel='noopener noreferrer'
+        className='w-full h-full mb-3 overflow-hidden relative cursor-pointer group block'
+      >
         <img
-          className="w-full h-full object-cover scale-100 group-hover:scale-110 duration-300 cursor-pointer"
+          className='w-full h-full object-cover scale-100 group-hover:scale-110 duration-300 cursor-pointer'
           src={image}
-          alt="ImageOne"
+          alt={title}
         />
-        <div className="w-full h-full absolute top-0 left-0 hover:bg-gradient-to-r from-green-600 via-green-600 to-green-200 opacity-20"></div>
-      </div>
-      <h3 className="font-titleFont text-lg font-semibold text-[#ccc]">
+        <div className='w-full h-full absolute top-0 left-0 hover:bg-gradient-to-r from-green-600 via-green-600 to-green-200 opacity-20'></div>
+      </a>
+      <h3 className='font-titleFont text-lg font-semibold text-[#ccc]'>
         {title}
       </h3>
-      <p className="text-base text-gray-400 -mt-1">{category}</p>
+      <p className='text-base text-gray-400 -mt-1'>{category}</p>
     </div>
   );
 };
